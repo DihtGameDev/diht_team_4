7ca7a4f9ba1ab0ee8a8e378c89540fac21928e34
wordedword75049 <naik.iva@gmail.com>
2020-05-21 20:03:42 +0300

    Added Canvas and even text changing corre

---
+++ Assets/Scripts/CanvasFields.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasFields : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Text goldText;
+    [SerializeField]
+    private UnityEngine.UI.Text farmscountText;
+
+    public UnityEngine.UI.Text FarmsCountText => farmscountText;
+
+    
+    public UnityEngine.UI.Text GoldText => goldText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
